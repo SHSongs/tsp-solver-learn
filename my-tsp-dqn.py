c8d20c0b00908e4633963feb57138a2ee083641d
Song <217.Songs.SH@gmail.com>
2021-06-04 09:23:17 +0900

    학습 성공, random policy action space 범위 error 수정,

--- my-tsp-dqn.py
+++ my-tsp-dqn.py
@@ -8,7 +8,7 @@ import random
 
 learning_rate = 0.005
 
-gamma = 0.98
+gamma = 0.90
 buffer_limit = 50000
 batch_size = 32
 
@@ -37,8 +37,7 @@ class Qnet(nn.Module):
         out = self.forward(obs)
         coin = random.random()
         if coin < epsilon:
-            # print('ramdom')
-            return random.randint(0, 1), out
+            return np.random.randint(0, 4), out
         else:
             return out.argmin().item(), out
 
@@ -52,6 +51,8 @@ def preprocessing(seq, state):
 def main():
     q = Qnet()
     optimizer = optim.Adam(q.parameters(), lr=learning_rate)
+    criterion = F.smooth_l1_loss
+
     print('시작')
     for i in range(num_episodes):
         done = False
@@ -61,13 +62,14 @@ def main():
         togo = [1, 2, 3]
 
         cnt = 0
+        # e = 0.85 if i < 0.98 * num_episodes else 0.01
         e = max(0.01, 0.20 - 0.01 * (i / 100))  # Linear annealing from 20% to 1%
 
         while not done:
             cnt += 1
             s_in = torch.from_numpy(preprocessing(seq, state)).float()
             a, out = q.sample_action(s_in, e)
-
+            # print(a, out)
             if a in togo:
                 seq.append(a)
                 togo.remove(a)
@@ -83,27 +85,32 @@ def main():
                 else:
                     next_out_pos = next_out.take(torch.tensor([0]))
 
-                next_q = torch.amin(next_out_pos)
-                target = r + gamma * next_q
+                next_min_q = torch.amin(next_out_pos)
+                target = r + gamma * next_min_q
                 # out[a] = target
 
-                loss = F.smooth_l1_loss(out[a], target)
+                loss = criterion(out[a], target)
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
+
+                print(' ', len(seq) - 1, q(s_in))
 
                 state = a
-
-
             elif a == 0 or a == state:  # 제자리 이동 또는 0으로의 이동 방지
-                target = 20.0
-                out[a] = target
+                target = torch.tensor(20.0).float()
 
-                loss = F.smooth_l1_loss(out[a], torch.tensor(target).float())
+                loss = criterion(out[a], target)
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
-
+            # else:
+            #     target = torch.tensor(5.0).float()
+            #
+            #     loss = criterion(out[a], target)
+            #     optimizer.zero_grad()
+            #     loss.backward()
+            #     optimizer.step()
             if len(seq) == 4:
                 done = True
 
@@ -113,19 +120,75 @@ def main():
 
         a = 0
         r = time[state][a]
-        total_reward += r
         target = r
+        total_reward += r
 
-        seq.append(a)
-
-        loss = F.smooth_l1_loss(out[a], torch.tensor(target).float())
+        loss = criterion(out[a], torch.tensor(target).float())
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-        print(cnt, total_reward)
-        print(seq)
+        seq.append(a)
+        print(' ', len(seq) - 1, q(s_in))
+
+        print(i, seq, total_reward, 'cnt', cnt)
 
 
 if __name__ == '__main__':
     main()
+
+
+
+##  Output
+#
+# 1989 [0, 2, 3, 1, 0] 16 cnt 3
+#   1 tensor([17.4692, 17.7654, 14.5070, 15.2014], grad_fn=<AddBackward0>)
+#   2 tensor([18.2226, 14.2752, 20.4083,  7.2300], grad_fn=<AddBackward0>)
+#   3 tensor([19.7000,  4.7000, 10.7401, 20.6567], grad_fn=<AddBackward0>)
+#   4 tensor([3.0000, 2.4411, 2.3192, 1.8865], grad_fn=<AddBackward0>)
+# 1990 [0, 2, 3, 1, 0] 16 cnt 3
+#   1 tensor([17.4692, 17.7654, 14.5070, 15.2014], grad_fn=<AddBackward0>)
+#   2 tensor([18.2226, 14.2752, 20.4083,  7.2300], grad_fn=<AddBackward0>)
+#   3 tensor([19.7000,  4.7000, 10.7401, 20.6567], grad_fn=<AddBackward0>)
+#   4 tensor([3.0000, 2.4411, 2.3192, 1.8865], grad_fn=<AddBackward0>)
+# 1991 [0, 2, 3, 1, 0] 16 cnt 3
+#   1 tensor([17.4692, 17.7654, 14.5070, 15.2014], grad_fn=<AddBackward0>)
+#   2 tensor([18.2226, 14.2752, 20.4083,  7.2300], grad_fn=<AddBackward0>)
+#   3 tensor([19.7000,  4.7000, 10.7401, 20.6567], grad_fn=<AddBackward0>)
+#   4 tensor([3.0000, 2.4411, 2.3192, 1.8865], grad_fn=<AddBackward0>)
+# 1992 [0, 2, 3, 1, 0] 16 cnt 3
+#   1 tensor([17.4692, 17.7654, 14.5070, 15.2014], grad_fn=<AddBackward0>)
+#   2 tensor([18.2226, 14.2752, 20.4083,  7.2300], grad_fn=<AddBackward0>)
+#   3 tensor([19.7000,  4.7000, 10.7401, 20.6567], grad_fn=<AddBackward0>)
+#   4 tensor([3.0000, 2.4411, 2.3192, 1.8865], grad_fn=<AddBackward0>)
+# 1993 [0, 2, 3, 1, 0] 16 cnt 3
+#   1 tensor([17.4692, 17.7654, 14.5070, 15.2014], grad_fn=<AddBackward0>)
+#   2 tensor([18.2226, 14.2752, 20.4083,  7.2300], grad_fn=<AddBackward0>)
+#   3 tensor([19.7000,  4.7000, 10.7401, 20.6567], grad_fn=<AddBackward0>)
+#   4 tensor([3.0000, 2.4411, 2.3192, 1.8865], grad_fn=<AddBackward0>)
+# 1994 [0, 2, 3, 1, 0] 16 cnt 3
+#   1 tensor([17.4692, 17.7654, 14.5070, 15.2014], grad_fn=<AddBackward0>)
+#   2 tensor([18.2226, 14.2752, 20.4083,  7.2300], grad_fn=<AddBackward0>)
+#   3 tensor([19.7000,  4.7000, 10.7401, 20.6567], grad_fn=<AddBackward0>)
+#   4 tensor([3.0000, 2.4411, 2.3192, 1.8865], grad_fn=<AddBackward0>)
+# 1995 [0, 2, 3, 1, 0] 16 cnt 3
+#   1 tensor([17.4692, 17.7654, 14.5070, 15.2014], grad_fn=<AddBackward0>)
+#   2 tensor([18.2226, 14.2752, 20.4083,  7.2300], grad_fn=<AddBackward0>)
+#   3 tensor([19.7000,  4.7000, 10.7401, 20.6567], grad_fn=<AddBackward0>)
+#   4 tensor([3.0000, 2.4411, 2.3192, 1.8865], grad_fn=<AddBackward0>)
+# 1996 [0, 2, 3, 1, 0] 16 cnt 3
+#   1 tensor([17.4692, 17.7654, 14.5070, 15.2014], grad_fn=<AddBackward0>)
+#   2 tensor([18.2226, 14.2752, 20.4083,  7.2300], grad_fn=<AddBackward0>)
+#   3 tensor([19.7000,  4.7000, 10.7401, 20.6567], grad_fn=<AddBackward0>)
+#   4 tensor([3.0000, 2.4411, 2.3192, 1.8865], grad_fn=<AddBackward0>)
+# 1997 [0, 2, 3, 1, 0] 16 cnt 3
+#   1 tensor([17.4692, 17.7654, 14.5070, 15.2014], grad_fn=<AddBackward0>)
+#   2 tensor([18.2226, 14.2752, 20.4083,  7.2300], grad_fn=<AddBackward0>)
+#   3 tensor([19.7000,  4.7000, 10.7401, 20.6567], grad_fn=<AddBackward0>)
+#   4 tensor([3.0000, 2.4411, 2.3192, 1.8865], grad_fn=<AddBackward0>)
+# 1998 [0, 2, 3, 1, 0] 16 cnt 3
+#   1 tensor([17.4692, 17.7654, 14.5070, 15.2014], grad_fn=<AddBackward0>)
+#   2 tensor([18.2226, 14.2752, 20.4083,  7.2300], grad_fn=<AddBackward0>)
+#   3 tensor([19.7000,  4.7000, 10.7401, 20.6567], grad_fn=<AddBackward0>)
+#   4 tensor([3.0000, 2.4411, 2.3192, 1.8865], grad_fn=<AddBackward0>)
+# 1999 [0, 2, 3, 1, 0] 16 cnt 3
