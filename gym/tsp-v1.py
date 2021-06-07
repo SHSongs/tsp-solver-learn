b0d9c503e29a9a3d2eae2254d77cf64b810dd4a8
Song <217.Songs.SH@gmail.com>
2021-06-07 21:01:57 +0900

    ptr net의 추론값으로 gym play

--- gym/tsp-v1.py
+++ gym/tsp-v1.py
@@ -1,41 +1,49 @@
-import elkai
 import or_gym
-import numpy as np
-# env = or_gym.make('TSP-v0')
-import matplotlib
+from solver import solver_RNN
+import torch
 
-env_config = {'N': 4}
+seq_len = 4
 
+env_config = {'N': seq_len}
 env = or_gym.make('TSP-v1', env_config=env_config)
 
-done = False
-s = env.reset()
+embedding_size = 128
+hidden_size = 128
 
-while s[0] != 0:
-    s = env.reset()
+model = solver_RNN(
+    embedding_size,
+    hidden_size,
+    seq_len,
+    2, 10)
+
+s = env.reset()
 
 print('coord')
 print(env.coords)
-matrix = env.distance_matrix
 
-print('distance matrix')
-print(matrix)
+coords = torch.FloatTensor(env.coords).transpose(1, 0).unsqueeze(0)
 
-result = elkai.solve_float_matrix(matrix)
-result = result[1:]
+rewards, log_probs, action = model(coords)
 
-print(result)
+action = action.squeeze(0).tolist()
 
-print('-----------------------\n\n')
+start_idx = action.index(s[0])
+a_1 = action[start_idx + 1:]
+a_2 = action[0:start_idx + 1]
+action = a_1 + a_2
 
 print('first state', s)
 
+total_reward = 0
 cnt = 0
+done = False
 while not done:
-    a = result[cnt]
+    a = action[cnt]
     next_state, reward, done, _ = env.step(a)
+    total_reward += reward
     print('current node', env.current_node)
-
     print(next_state, reward, done)
 
     cnt += 1
+
+print('total length', total_reward)
